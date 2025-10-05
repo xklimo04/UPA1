535885896dba333dcaee34708b40eb06cf80d6d1
Veronika Čalkovská <veronikacalkovska@Veronika--MacBook-Air.local>
2025-10-05 23:35:04 +0200

    feat: getting urls from first page

---
+++ get_urls.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from urllib.parse import urlsplit, urlunsplit, quote
+
+def encode_url(url: str) -> str:
+    parts = urlsplit(url)
+    path = quote(parts.path, safe="/%")
+    query = quote(parts.query, safe="=&%")
+    fragment = quote(parts.fragment, safe="")
+    return urlunsplit((parts.scheme, parts.netloc, path, query, fragment))
+
+
+page = urlopen("https://www.mobileshop.eu/cz/android-os/")
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+rows = soup.select("#mainContent")[0].find_all("h5")
+for row in rows:
+    href = row.find("a")['href']
+    href = "https://www.mobileshop.eu" + href 
+    print(encode_url(href))
+
+try: 
+    nextpage = soup.find("nav", class_ = "pager").find("a", string="DALŠÍ")['href']
+    print(nextpage)
+except Exception:
+    pass
