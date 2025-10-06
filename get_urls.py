b956aff4f9fa689c31ee0e4944a2c8cff0408a4f
Veronika Čalkovská <veronikacalkovska@dhcpg083.fit.vutbr.cz>
2025-10-06 10:24:11 +0200

    Add: browsing all pages with products

--- get_urls.py
+++ get_urls.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import urlsplit, urlunsplit, quote
 
+url = "https://www.mobileshop.eu/cz/android-os/"
+
 def encode_url(url: str) -> str:
     parts = urlsplit(url)
     path = quote(parts.path, safe="/%")
@@ -9,18 +11,18 @@ def encode_url(url: str) -> str:
     fragment = quote(parts.fragment, safe="")
     return urlunsplit((parts.scheme, parts.netloc, path, query, fragment))
 
+while(1):
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    rows = soup.select("#mainContent")[0].find_all("h5")
+    for row in rows:
+        href = row.find("a")['href']
+        href = "https://www.mobileshop.eu" + href 
+        print(encode_url(href))
 
-page = urlopen("https://www.mobileshop.eu/cz/android-os/")
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
-rows = soup.select("#mainContent")[0].find_all("h5")
-for row in rows:
-    href = row.find("a")['href']
-    href = "https://www.mobileshop.eu" + href 
-    print(encode_url(href))
-
-try: 
-    nextpage = soup.find("nav", class_ = "pager").find("a", string="DALŠÍ")['href']
-    print(nextpage)
-except Exception:
-    pass
+    try: 
+        nextpage = soup.find("nav", class_ = "pager").find("a", string="DALŠÍ")['href']
+        url = "https://www.mobileshop.eu" + nextpage
+    except Exception:
+        exit()
