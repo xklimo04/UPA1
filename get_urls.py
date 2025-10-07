51d95c930e99ddee38bed1614e67921c6e56693d
Veronika Čalkovská <veronikacalkovska@Veronika--MacBook-Air.local>
2025-10-07 22:28:42 +0200

    Fix: printing only 150 urls

--- get_urls.py
+++ get_urls.py
@@ -11,18 +11,27 @@ def encode_url(url: str) -> str:
     fragment = quote(parts.fragment, safe="")
     return urlunsplit((parts.scheme, parts.netloc, path, query, fragment))
 
-while(1):
+i = 0
+iterate = True
+
+while(iterate):
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
     rows = soup.select("#mainContent")[0].find_all("h5")
+
     for row in rows:
+        if 150 <= i:
+            iterate = not iterate
+            break
         href = row.find("a")['href']
         href = "https://www.mobileshop.eu" + href 
         print(encode_url(href))
+        i = i + 1
 
-    try: 
-        nextpage = soup.find("nav", class_ = "pager").find("a", string="DALŠÍ")['href']
-        url = "https://www.mobileshop.eu" + nextpage
-    except Exception:
-        exit()
+    if iterate:
+        try: 
+            nextpage = soup.find("nav", class_ = "pager").find("a", string="DALŠÍ")['href']
+            url = "https://www.mobileshop.eu" + nextpage
+        except:
+            iterate = not iterate
